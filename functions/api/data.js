7d93cf230f22e88f77dc6e5f98730e9acf765d0e
Abdul Rafiu <157203422+AbdulRafiu0@users.noreply.github.com>
2026-07-18 03:37:15 +0500

    Delete functions/api directory

--- functions/api/data.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify({ message: "Hello from Functions!" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
